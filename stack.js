0c7466c2f7ef5f14f4f47c93702cc8299c412534
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-11 08:49:56 -0800

    added implementation of stack

---
+++ stack.js
@@ -0,0 +1,21 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    add(item) {
+        this.items.push(item);
+    }
+
+    pop() {
+        return this.items.pop();
+    }
+
+    isEmpty() {
+        return this.items.length <= 0;
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+}
