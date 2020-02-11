8ffc2df145f84d139acb986da209ddcd1d21c953
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-11 09:46:05 -0800

    working on algorithms implementation in graph

--- graph.js
+++ graph.js
@@ -1,23 +1,65 @@
-import stack from './stack';
-import queue from './queue';
+import Stack from './stack';
+import Queue from './queue';
 
 class Graph {
     constructor() {
         this.vertices = {};
     }
 
-    add_vertex(vertex) {
+    addVertex(vertex) {
         if(vertex in this.vertices) {
             throw new Error('That vertex already exists!')
         }
         this.vertices[vertex] = new Set();
     }
 
-    add_edge(v1, v2) {
+    addEdge(v1, v2) {
         if(v1 in this.vertices && v2 in this.vertices) {
             this.vertices[v1].add(v2)
         } else {
             throw new Error("Can not create an edge based on given vertices!");
+        }
+    }
+
+    bfsPrint(startingVertex) {
+        const queue = new Queue();
+
+        const visitedVertices = new Set();
+
+        queue.enqueue(startingVertex);
+
+        while(!queue.isEmpty()) {
+            let vertex = queue.dequeue();
+
+            if(!visitedVertices.has(vertex)) {
+                visitedVertices.add(vertex);
+                console.log(vertex);
+
+                for(let nextVert of this.vertices[vertex]) {
+                    queue.enqueue(nextVert);
+                }
+            }
+        }
+    }
+
+    dfsPrint(startingVertex) {
+        const stack = new Stack();
+
+        const visitedVertices = new Set();
+
+        stack.add(startingVertex);
+
+        while(!stack.isEmpty()) {
+            let vertex = stack.pop();
+
+            if(!visitedVertices.has(vertex)) {
+                visitedVertices.add(vertex);
+                console.log(vertex)
+
+                for(let nextVert of this.vertices[vertex]) {
+                    stack.add(nextVert)
+                }
+            }
         }
     }
 }
